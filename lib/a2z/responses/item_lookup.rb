bd38193f2460c67b915515bac71d0fa41b3a739f
Gustav Jonsson <gustav@invoke.se>
2014-01-16 15:59:41 +0100

    ItemLookup response supports multiple items

--- lib/a2z/responses/item_lookup.rb
+++ lib/a2z/responses/item_lookup.rb
@@ -1,26 +1,30 @@
 module A2z
   module Responses
     class ItemLookup
-      attr_accessor :operation_request, :item
-      
+      attr_accessor :operation_request, :items
+
       def initialize
         @valid = true
       end
-      
+
       def valid=(value)
         @valid = !!value
       end
-      
+
       def valid?
         @valid
       end
-      
+
+      def item
+        items.first
+      end
+
       # TODO capture data['Items']['Request']['Errors'] into an attr_accessor value
       # TODO consider capturing data['Items']['Request'] into an attr_accessor value
       def self.from_response(data)
         new.tap do |item_lookup|
           item_lookup.operation_request = OperationRequest.from_response(data['OperationRequest']) if data['OperationRequest']
-          item_lookup.item              = Item.from_response(data['Items']['Item']) if data['Items'] && data['Items']['Item']
+          item_lookup.items             = [data['Items']['Item']].flatten.map { |x| Item.from_response(x) } if data['Items'] && data['Items']['Item']
           item_lookup.valid             = data['Items']['Request']['IsValid'] == 'True' rescue false
           item_lookup.freeze
         end
