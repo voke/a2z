f772a7759762c609a434f71077cbcef578db4fd9
Gustav Jonsson <gustav@invoke.se>
2014-04-02 17:54:36 +0200

    Use latest version of jeff and replace deprecated accessors

--- a2z.gemspec
+++ a2z.gemspec
@@ -7,18 +7,18 @@ Gem::Specification.new do |gem|
   gem.description   = 'Ruby DSL for Amazon Product Advertising API'
   gem.summary       = 'Ruby DSL for Amazon Product Advertising API'
   gem.homepage      = 'https://github.com/mhuggins/a2z'
-  
+
   gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = 'a2z'
   gem.require_path  = 'lib'
   gem.version       = A2z::VERSION
-  
+
   gem.add_dependency 'crack'
-  gem.add_dependency 'jeff', '~> 0.6.4'
+  gem.add_dependency 'jeff', '~> 1.0.1'
   gem.add_dependency 'money'
-  
+
   gem.add_development_dependency 'rake'
   gem.add_development_dependency 'rspec'
 end
--- lib/a2z/client.rb
+++ lib/a2z/client.rb
@@ -4,11 +4,11 @@ require 'crack/xml'
 module A2z
   class Client
     ErrorResponse = Class.new(ArgumentError)
-    
+
     include Jeff
-    
+
     attr_reader :country, :tag
-    
+
     HOSTS = {
         ca: 'ecs.amazonaws.ca',
         cn: 'webservices.amazon.cn',
@@ -20,49 +20,49 @@ module A2z
         uk: 'ecs.amazonaws.co.uk',
         us: 'ecs.amazonaws.com',
     }.freeze
-    
+
     params 'AssociateTag' => -> { tag },
            'Service'      => 'AWSECommerceService',
            'Version'      => '2011-08-01'
-    
+
     def initialize(opts = {})
-      self.country  = opts.fetch(:country, :us)
-      self.key      = opts[:key]
-      self.secret   = opts[:secret]
-      self.tag      = opts[:tag]
+      self.country               = opts.fetch(:country, :us)
+      self.aws_access_key_id     = opts[:key]
+      self.aws_secret_access_key = opts[:secret]
+      self.tag                   = opts[:tag]
     end
-    
+
     def country=(code)
       raise ArgumentError.new("Country code must be one of #{HOSTS.keys.join(', ')}.") if code.nil? || HOSTS[code.to_sym].nil?
       @country = code.to_sym
       @endpoint = "http://#{HOSTS[@country]}/onca/xml"
     end
-    
+
     def item_search(&block)
       response = request(Requests::ItemSearch.new(&block))
       Responses::ItemSearch.from_response(response)
     end
-    
+
     def item_lookup(&block)
       response = request(Requests::ItemLookup.new(&block))
       Responses::ItemLookup.from_response(response)
     end
-    
+
     def browse_node_lookup(id, &block)
       response = request(Requests::BrowseNodeLookup.new(id, &block))
       Responses::BrowseNodeLookup.from_response(response)
     end
-    
+
     private
-    
+
     def tag=(tag)
       @tag = tag
     end
-    
+
     def request(req)
       response = get(query: req.params)
       response = Crack::XML.parse(response.body)
-      
+
       # strip the root xml node
       response.values.first
     end
